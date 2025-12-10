e02423331d425f2a7529409db585cfefe795deb8
cpage275 <48229881+cpage275@users.noreply.github.com>
2025-12-10 00:13:18 -0500

    fixing vite

--- vite.config.js
+++ vite.config.js
@@ -2,7 +2,7 @@ import { defineConfig } from 'vite'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: './', // Use relative paths for assets
+  base: '/canvas-final-beetles/', // GitHub Pages repo name
   build: {
     outDir: 'dist',
     assetsDir: 'assets',
