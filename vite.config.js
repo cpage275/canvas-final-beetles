f5949c5c64e50f1874e05c02fa2f879ae8e4189d
cpage275 <48229881+cpage275@users.noreply.github.com>
2025-12-10 00:06:05 -0500

    fixing vite

---
+++ vite.config.js
@@ -0,0 +1,10 @@
+import { defineConfig } from 'vite'
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  base: './', // Use relative paths for assets
+  build: {
+    outDir: 'dist',
+    assetsDir: 'assets',
+  }
+})
